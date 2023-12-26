ca5d9b19adcdd5a4c19c8a2826ff500814127967
shree <shreesbadiger@gmail.com>
2023-12-26 18:54:50 +0530

    updated

---
+++ jenkins-ip-updated/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-remote-d76"
+    key    = "misc/jenkins-ip-updated/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_instance" "jenkins"{
+    instance_id = i-073d95a812f8e4d01
+}
+
+resource "aws_route53_record" "jenkins" {
+  name = "jenkins"
+  type = "A"
+  ttl = 10
+  zone_id = "Z043769343BOX5323WQF"
+  records = [data.aws_instance.jenkins.public_ip]
+}
